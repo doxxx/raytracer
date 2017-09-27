87518ad0015ef8d1740ae506317cbef9ff461a4c
Gordon Tyler <gordon@doxxx.net>
2017-09-27 00:10:48 -0400

    Refactor diffuse calculations.

--- src/shaders/diffuse.rs
+++ src/shaders/diffuse.rs
@@ -1,9 +1,11 @@
+use std::f64::consts::PI;
+
 use shaders::Shader;
 
 use color::Color;
-use direction::Dot;
+use direction::{Direction, Dot};
 use system::{RenderContext, Ray, SurfaceInfo};
-use texture::{ColorSource,Texture};
+use texture::{ColorSource, Texture};
 
 pub const DEFAULT_ALBEDO: f64 = 0.18;
 
@@ -16,9 +18,20 @@ pub struct Diffuse {
     pub highlight: f64,
 }
 
+impl Diffuse {
+    fn lambertian(&self, dir: Direction, si: &SurfaceInfo) -> f64 {
+        si.n.dot(-dir).max(0.0) * self.albedo
+    }
+
+    fn phong(&self, dir: Direction, si: &SurfaceInfo) -> f64 {
+        let r = dir.reflect(si.n);
+        r.dot(-dir).max(0.0).powf(self.highlight)
+    }
+}
+
 impl Shader for Diffuse {
     fn shade_point(&self, context: &RenderContext, si: &SurfaceInfo) -> Color {
-        let mut lambertian = Color::black();
+        let mut diffuse = Color::black();
         let mut specular = Color::black();
 
         for light in &context.scene.lights {
@@ -26,16 +39,13 @@ impl Shader for Diffuse {
             let shadow_ray = Ray::shadow(si.point + si.n * context.options.bias, -dir, 0);
 
             if shadow_ray.trace(&context.scene.objects, distance).is_none() {
-                let dot = si.n.dot(-dir).max(0.0);
-                if dot > 0.0 {
-                    lambertian += self.texture.color_at_uv(si.uv) * self.albedo * intensity * dot;
-                }
-                let r = dir.reflect(si.n);
-                specular += intensity * r.dot(-dir).max(0.0).powf(self.highlight); // todo: specular color
+                let surface_color = self.texture.color_at_uv(si.uv);
+                diffuse += surface_color * intensity * self.lambertian(dir, si);
+                specular += intensity * self.phong(dir, si);
             }
         }
 
-        lambertian * self.diffuse_factor + specular * self.specular_factor
+        diffuse * self.diffuse_factor + specular * self.specular_factor
     }
 
     fn box_clone(&self) -> Box<Shader> {
