ebe1696f875138ea483d39502f7513e72af6189f
Gordon Tyler <gordon@doxxx.net>
2018-05-19 19:56:41 -0400

    Fix plane/rect bidirectionality.

--- src/shapes/plane.rs
+++ src/shapes/plane.rs
@@ -18,18 +18,18 @@ fn plane_uv(n: Direction) -> (Direction, Direction) {
     (u, v)
 }
 
-fn plane_intersect(o: Point, n: Direction, ray: &Ray, bidi: bool) -> Option<f64> {
+fn plane_intersect(o: Point, n: Direction, ray: &Ray) -> Option<f64> {
     let denom = n.dot(ray.direction);
     if denom.abs() < 1e-6 {
         return None;
     }
     let w = ray.origin - o;
     let ndotw = n.dot(w);
-    if !bidi && ndotw < 0.0 {
+    if ndotw < 0.0 {
         return None;
     }
     let t = -ndotw / denom;
-    if t < 0.0 {
+     if t < 0.0 {
         return None;
     }
     Some(t)
@@ -38,144 +38,123 @@ fn plane_intersect(o: Point, n: Direction, ray: &Ray, bidi: bool) -> Option<f64>
 pub struct Plane {
     origin: Point,
     normal: Direction,
+    reverse_normal: Direction,
     bidi: bool,
-    u: Direction,
-    v: Direction,
+    uv: (Direction, Direction),
+    reverse_uv: (Direction, Direction),
 }
 
 impl Plane {
     pub fn new(origin: Point, normal: Direction, bidi: bool) -> Plane {
-        let (u, v) = plane_uv(normal);
+        let reverse_normal = normal * -1.0;
+        let uv = plane_uv(normal);
+        let reverse_uv = plane_uv(reverse_normal);
         Plane {
             origin,
             normal,
+            reverse_normal,
             bidi,
-            u,
-            v,
+            uv,
+            reverse_uv,
         }
     }
-}
 
-impl Intersectable for Plane {
-    fn intersect(&self, ray: &Ray) -> Option<Intersection> {
-        if let Some(t) = plane_intersect(self.origin, self.normal, ray, self.bidi) {
+    fn bidi_intersect_with_bounds<F>(&self, ray: &Ray, out_of_bounds: F) -> Option<Intersection> 
+        where F: FnOnce(&Point) -> bool 
+    {
+        let mut n = self.normal;
+        let mut uv = self.uv;
+        let mut t = plane_intersect(self.origin, n, ray);
+        if self.bidi && t.is_none() {
+            n = self.reverse_normal;
+            uv = self.reverse_uv;
+            t = plane_intersect(self.origin, n, ray);
+        }
+        if let Some(t) = t {
             let p = ray.origin + ray.direction * t;
+            if out_of_bounds(&p) {
+                return None;
+            }
             let op = p - self.origin;
-            let uv = Vector2f(self.u.dot(op), self.v.dot(op));
-            return Some(Intersection { t, n: self.normal, uv });
+            let uv = Vector2f(uv.0.dot(op), uv.1.dot(op));
+            return Some(Intersection { t, n, uv });
         }
 
         None
     }
 }
 
-impl Shape for Plane {}
-
-fn xy_normal() -> Direction {
-    Direction::new(0.0, 0.0, 1.0)
+impl Intersectable for Plane {
+    fn intersect(&self, ray: &Ray) -> Option<Intersection> {
+        self.bidi_intersect_with_bounds(ray, |_| false)
+    }
 }
 
+impl Shape for Plane {}
+
 pub struct XYRectangle {
-    origin: Point,
-    bidi: bool,
+    plane: Plane,
     x0: f64,
     x1: f64,
     y0: f64,
     y1: f64,
-    u: Direction,
-    v: Direction,
 }
 
 impl XYRectangle {
     pub fn new(origin: Point, width: f64, height: f64, bidi: bool) -> XYRectangle {
-        let (u, v) = plane_uv(xy_normal());
+        let plane = Plane::new(origin, Direction::new(0.0, 0.0, 1.0), bidi);
         let x0 = origin.x - width / 2.0;
         let x1 = origin.x + width / 2.0;
         let y0 = origin.y - height / 2.0;
         let y1 = origin.y + height / 2.0;
 
         XYRectangle {
-            origin,
-            bidi,
+            plane,
             x0,
             x1,
             y0,
             y1,
-            u,
-            v,
         }
     }
 }
 
 impl Intersectable for XYRectangle {
     fn intersect(&self, ray: &Ray) -> Option<Intersection> {
-        let n = xy_normal();
-        if let Some(t) = plane_intersect(self.origin, n, ray, self.bidi) {
-            let p = ray.origin + ray.direction * t;
-            if p.x < self.x0 || p.x > self.x1 || p.y < self.y0 || p.y > self.y1 {
-                return None;
-            }
-            let op = p - self.origin;
-            let uv = Vector2f(self.u.dot(op), self.v.dot(op));
-            return Some(Intersection { t, n: n, uv });
-        }
-
-        None
+        self.plane.bidi_intersect_with_bounds(ray, |p| p.x < self.x0 || p.x > self.x1 || p.y < self.y0 || p.y > self.y1)
     }
 }
 
 impl Shape for XYRectangle {}
 
-fn xz_normal() -> Direction {
-    Direction::new(0.0, 1.0, 0.0)
-}
-
 pub struct XZRectangle {
-    origin: Point,
-    bidi: bool,
+    plane: Plane,
     x0: f64,
     x1: f64,
     z0: f64,
     z1: f64,
-    u: Direction,
-    v: Direction,
 }
 
 impl XZRectangle {
     pub fn new(origin: Point, width: f64, height: f64, bidi: bool) -> XZRectangle {
-        let (u, v) = plane_uv(xz_normal());
+        let plane = Plane::new(origin, Direction::new(0.0, 1.0, 0.0), bidi);
         let x0 = origin.x - (width / 2.0);
         let x1 = origin.x + width / 2.0;
         let z0 = origin.z - (height / 2.0);
         let z1 = origin.z + height / 2.0;
 
         XZRectangle {
-            origin,
-            bidi,
+            plane,
             x0,
             x1,
             z0,
             z1,
-            u,
-            v,
         }
     }
 }
 
 impl Intersectable for XZRectangle {
     fn intersect(&self, ray: &Ray) -> Option<Intersection> {
-        let n = xz_normal();
-        if let Some(t) = plane_intersect(self.origin, n, ray, self.bidi) {
-            let p = ray.origin + ray.direction * t;
-            if p.x < self.x0 || p.x > self.x1 || p.z < self.z0 || p.z > self.z1 {
-                return None;
-            }
-            let op = p - self.origin;
-            let uv = Vector2f(self.u.dot(op), self.v.dot(op));
-            return Some(Intersection { t, n: n, uv });
-        }
-
-        None
+        self.plane.bidi_intersect_with_bounds(ray, |p| p.x < self.x0 || p.x > self.x1 || p.z < self.z0 || p.z > self.z1)
     }
 }
 
@@ -186,51 +165,34 @@ fn zy_normal() -> Direction {
 }
 
 pub struct ZYRectangle {
-    origin: Point,
-    bidi: bool,
+    plane: Plane,
     z0: f64,
     z1: f64,
     y0: f64,
     y1: f64,
-    u: Direction,
-    v: Direction,
 }
 
 impl ZYRectangle {
     pub fn new(origin: Point, width: f64, height: f64, bidi: bool) -> ZYRectangle {
-        let (u, v) = plane_uv(zy_normal());
+        let plane = Plane::new(origin, Direction::new(1.0, 0.0, 0.0), bidi);
         let z0 = origin.z - (width / 2.0);
         let z1 = origin.z + width / 2.0;
         let y0 = origin.y - (height / 2.0);
         let y1 = origin.y + height / 2.0;
 
         ZYRectangle {
-            origin,
-            bidi,
+            plane,
             z0,
             z1,
             y0,
             y1,
-            u,
-            v,
         }
     }
 }
 
 impl Intersectable for ZYRectangle {
     fn intersect(&self, ray: &Ray) -> Option<Intersection> {
-        let n = zy_normal();
-        if let Some(t) = plane_intersect(self.origin, n, ray, self.bidi) {
-            let p = ray.origin + ray.direction * t;
-            if p.z < self.z0 || p.z > self.z1 || p.y < self.y0 || p.y > self.y1 {
-                return None;
-            }
-            let op = p - self.origin;
-            let uv = Vector2f(self.u.dot(op), self.v.dot(op));
-            return Some(Intersection { t, n: n, uv });
-        }
-
-        None
+        self.plane.bidi_intersect_with_bounds(ray, |p| p.z < self.z0 || p.z > self.z1 || p.y < self.y0 || p.y > self.y1)
     }
 }
 
