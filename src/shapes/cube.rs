9248100e51d71c8161d670bb70eeed9747853056
Gordon Tyler <gordon@doxxx.net>
2018-05-19 18:01:36 -0400

    Add cube shape.

---
+++ src/shapes/cube.rs
@@ -0,0 +1,64 @@
+use super::{XYRectangle, XZRectangle, ZYRectangle};
+use point::Point;
+use shapes::Shape;
+use system::{Intersectable, Intersection, Ray};
+
+pub struct Cube {
+    sides: [Box<Shape>; 6],
+}
+
+impl Cube {
+    pub fn new(p1: Point, p2: Point) -> Cube {
+        let min_x = p1.x.min(p2.x);
+        let min_y = p1.y.min(p2.y);
+        let min_z = p1.z.min(p2.z);
+        let max_x = p1.x.max(p2.x);
+        let max_y = p1.y.max(p2.y);
+        let max_z = p1.z.max(p2.z);
+        Cube {
+            sides: [
+                xyrect(min_x, min_y, max_x, max_y, min_z),
+                xyrect(min_x, min_y, max_x, max_y, max_z),
+                xzrect(min_x, min_z, max_x, max_z, min_y),
+                xzrect(min_x, min_z, max_x, max_z, max_y),
+                zyrect(min_z, min_y, max_z, max_y, min_x),
+                zyrect(min_z, min_y, max_z, max_y, max_x),
+            ],
+        }
+    }
+}
+
+fn xyrect(x0: f64, y0: f64, x1: f64, y1: f64, z: f64) -> Box<Shape> {
+    Box::new(XYRectangle::new(
+        Point::new((x1 - x0) / 2.0 + x0, (y1 - y0) / 2.0 + y0, z),
+        x1 - x0,
+        y1 - y0,
+        true,
+    ))
+}
+
+fn xzrect(x0: f64, z0: f64, x1: f64, z1: f64, y: f64) -> Box<Shape> {
+    Box::new(XZRectangle::new(
+        Point::new((x1 - x0) / 2.0 + x0, y, (z1 - z0) / 2.0 + z0),
+        x1 - x0,
+        z1 - z0,
+        true,
+    ))
+}
+
+fn zyrect(z0: f64, y0: f64, z1: f64, y1: f64, x: f64) -> Box<Shape> {
+    Box::new(ZYRectangle::new(
+        Point::new(x, (y1 - y0) / 2.0 + y0, (z1 - z0) / 2.0 + z0),
+        z1 - z0,
+        y1 - y0,
+        true,
+    ))
+}
+
+impl Intersectable for Cube {
+    fn intersect(&self, ray: &Ray) -> Option<Intersection> {
+        self.sides.intersect(ray)
+    }
+}
+
+impl Shape for Cube {}
--- src/shapes/mod.rs
+++ src/shapes/mod.rs
@@ -1,15 +1,35 @@
+use system::Intersection;
+use system::Ray;
 use system::Intersectable;
 
 pub mod bounding_box;
+pub mod cube;
 pub mod composite;
 pub mod mesh;
 pub mod plane;
 pub mod sphere;
 
 pub use self::bounding_box::*;
+pub use self::cube::*;
 pub use self::composite::*;
 pub use self::mesh::*;
 pub use self::plane::*;
 pub use self::sphere::*;
 
 pub trait Shape: Intersectable + Send + Sync {}
+
+impl Intersectable for [Box<Shape>] {
+    fn intersect(&self, ray: &Ray) -> Option<Intersection> {
+        let mut nearest: Option<Intersection> = None;
+
+        for s in self {
+            if let Some(i) = s.intersect(ray) {
+                if nearest.is_none() || i.t < nearest.as_ref().unwrap().t {
+                    nearest = Some(i);
+                }
+            }
+        }
+        
+        nearest
+    }
+}
