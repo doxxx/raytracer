67fc887aec1f86d9bc477d6ebd9f992c58ad5b65
Gordon Tyler <gordon@doxxx.net>
2018-06-27 20:47:47 -0400

    Add cylinder shape.

---
+++ src/shapes/cylinder.rs
@@ -0,0 +1,150 @@
+use std::f64;
+use std::mem;
+
+use crate::direction::{Direction, Dot};
+use crate::matrix::Matrix44f;
+use crate::object::Transformation;
+use crate::point::Point;
+use crate::shapes::{Interval, Plane, Shape};
+use crate::system::{Intersectable, Intersection, Ray, Transformable};
+use crate::vector::Vector2f;
+
+pub struct Cylinder {
+    origin: Point,
+    radius: f64,
+    height: f64,
+    tx: Transformation,
+}
+
+impl Cylinder {
+    pub fn new(origin: Point, radius: f64, height: f64) -> Cylinder {
+        Cylinder {
+            origin,
+            radius,
+            height,
+            tx: Transformation::new(),
+        }
+    }
+
+    fn side_intersection(&self, o: Point, d: Direction, t: f64, y: f64) -> Intersection {
+        let p = o + d * t;
+        let n = Direction::new(p.x, 0.0, p.z).normalize();
+        let u = (1.0 - n.z.atan2(n.x) / f64::consts::PI) * 0.5;
+        let max_y = self.height / 2.0;
+        let v = 1.0 - (y + max_y) / self.height;
+
+        Intersection {
+            t,
+            n,
+            uv: Vector2f(u, v),
+        }
+    }
+}
+
+impl Intersectable for Cylinder {
+    fn intersect(&self, ray: &Ray) -> Option<Intersection> {
+        super::first_positive_intersection(self.intersection_intervals(ray))
+    }
+}
+
+impl Shape for Cylinder {
+    fn transform(&mut self, m: Matrix44f) {
+        self.tx.transform(m);
+    }
+
+    fn transformation(&self) -> &Transformation {
+        &self.tx
+    }
+
+    fn intersection_intervals(&self, ray: &Ray) -> Vec<Interval> {
+        let o = ray.origin;
+        let d = ray.direction;
+        let a = d.x.powi(2) + d.z.powi(2);
+        let b = 2.0 * o.x * d.x + 2.0 * o.z * d.z;
+        let c = o.x.powi(2) + o.z.powi(2) - self.radius.powi(2);
+        let discr = b.powi(2) - 4.0 * a * c;
+        if discr < 0.0 {
+            return Vec::with_capacity(0);
+        }
+
+        let sqrt = discr.sqrt();
+        let mut t0 = (-b + sqrt) / (2.0 * a);
+        let mut t1 = (-b - sqrt) / (2.0 * a);
+
+        if t0 > t1 {
+            mem::swap(&mut t0, &mut t1);
+        }
+
+        let y0 = o.y + t0 * d.y;
+        let y1 = o.y + t1 * d.y;
+
+        // figure out which sides have been hit
+        let max_y = self.height / 2.0;
+        let min_y = -max_y;
+        let top_cap = Plane::new(Point::new(0.0, max_y, 0.0), Direction::new(0.0, 1.0, 0.0));
+        let bottom_cap = Plane::new(Point::new(0.0, min_y, 0.0), Direction::new(0.0, -1.0, 0.0));
+
+        if y0 < min_y && y1 < min_y || y0 > max_y && y1 > max_y {
+            // miss
+            Vec::with_capacity(0)
+        } else if y0 > max_y {
+            // top cap
+            let Interval(top_i, _) = top_cap
+                .intersection_intervals(ray)
+                .pop()
+                .expect("expected one interval from top cap intersection");
+            if y1 > min_y && y1 < max_y {
+                // and back side
+                vec![Interval(top_i, self.side_intersection(o, d, t1, y1))]
+            } else {
+                // and bottom cap
+                assert!(y1 < min_y);
+                let Interval(bottom_i, _) = bottom_cap
+                    .intersection_intervals(ray)
+                    .pop()
+                    .expect("expected one interval from bottom cap intersection");
+                vec![Interval(top_i, bottom_i)]
+            }
+        } else if y0 < min_y {
+            // bottom cap
+            let Interval(bottom_i, _) = bottom_cap
+                .intersection_intervals(ray)
+                .pop()
+                .expect("expected one interval from bottom cap intersection");
+            if y1 > min_y && y1 < max_y {
+                // and back side
+                vec![Interval(bottom_i, self.side_intersection(o, d, t1, y1))]
+            } else {
+                // and top cap
+                assert!(y1 > max_y);
+                let Interval(top_i, _) = top_cap
+                    .intersection_intervals(ray)
+                    .pop()
+                    .expect("expected one interval from top cap intersection");
+                vec![Interval(bottom_i, top_i)]
+            }
+        } else {
+            // front side
+            let front_i = self.side_intersection(o, d, t0, y0);
+            if y1 > min_y && y1 < max_y {
+                // and back side
+                vec![Interval(front_i, self.side_intersection(o, d, t1, y1))]
+            } else if y1 < min_y {
+                // and bottom cap
+                let Interval(bottom_i, _) = bottom_cap
+                    .intersection_intervals(ray)
+                    .pop()
+                    .expect("expected one interval from bottom cap intersection");
+                vec![Interval(front_i, bottom_i)]
+            } else {
+                // and top cap
+                assert!(y1 > max_y);
+                let Interval(top_i, _) = top_cap
+                    .intersection_intervals(ray)
+                    .pop()
+                    .expect("expected one interval from top cap intersection");
+                vec![Interval(front_i, top_i)]
+            }
+        }
+    }
+}
